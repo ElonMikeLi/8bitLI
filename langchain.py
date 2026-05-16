2690a45bf4636d7e8186c714746453c1311aa60e
Elon mike <2419860642@qq.com>
2026-05-16 16:59:03 +0800

    更新 langchain.py

--- langchain.py
+++ langchain.py
@@ -1,1 +1,22 @@
 # this is a project about langchain !
+
+# print('hellow world')
+
+import os
+from openai import OpenAI
+
+client = OpenAI(
+	api_key= os.environ.get('API_key'),
+    base_url="https://api.deepseek.com"
+)
+
+response = client.chat.completions.create(
+	model='deepseek-chat',
+    messages=[
+    	{"role": "system", "content":"你是我的私人助理！"}，
+    	{"role": "user", "content": "你好，你是干嘛的！"}
+    ],
+    stream = false
+)
+
+print(response.choices[0].message.content)
